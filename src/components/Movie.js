a831648b5e25acf3261754fd32645a4786939ca7
SIEUN KIM <sieun043@gmail.com>
2026-05-31 19:35:47 +0900

    test deploy

---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+function Movie({coverImg, title, summary, genres, id}) {
+
+    return (
+    <div>
+        <img src={coverImg} alt={title} />
+        <h2><Link to={`/movie/${id}`}>title</Link></h2>
+        <p>{summary}</p>
+        <ul>
+            {genres.map((genre, index) => (
+              <li key={index}>genre</li>
+            ))}
+        </ul>
+    </div>
+    );
+}
+export default Movie;
